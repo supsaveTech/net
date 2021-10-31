e7cb1dc658a5c8e601cd410547c208a8badec399
Mitchel Charles (SupsaveTech) <63431390+supsaveTech@users.noreply.github.com>
2021-10-31 10:20:42 +0100

    Update index.js

--- src/sections/teams/index.js
+++ src/sections/teams/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Img1 from "../../assets/images/22.png";
 import Img2 from "../../assets/images/34.png";
 import Img3 from "../../assets/images/27.png";
+import Twitter from "../../assets/images/twitter.png";
 import "./style.css";
 const Index = () => {
   return (
@@ -16,11 +17,24 @@ const Index = () => {
               <div className="img-wrapper">
                 <img src={Img1} alt="" className="img-fluid" />
               </div>
-              <p>Founder</p>
-              <span>
-                {/* Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Aliquam corrupti similique. */}
-              </span>
+              <p className="social-icon">Founder &nbsp;
+
+              {/* <div className="social-icons"> */}
+          <a
+            href="https://twitter.com/goosepunks"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={Twitter} alt="twitter" />
+          </a>
+           
+        {/* </div> */}
+ 
+              </p>
+              {/* <span>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Aliquam corrupti similique.
+              </span> */}
             </div>
           </div>
           <div className="col-12 col-md-4">
@@ -28,7 +42,16 @@ const Index = () => {
               <div className="img-wrapper">
                 <img src={Img2} alt="" className="img-fluid" />
               </div>
-              <p>Co-Founder</p>
+              <p className="social-icon">Community Manager &nbsp;
+              
+              <a
+            href="https://twitter.com/goosepunks"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={Twitter} alt="twitter" />
+          </a>
+              </p>
               <span>
                 {/* Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                 Aliquam corrupti similique. */}
@@ -40,7 +63,17 @@ const Index = () => {
               <div className="img-wrapper">
                 <img src={Img3} alt="" className="img-fluid" />
               </div>
-              <p>CFO</p>
+              <p className="social-icon">Robin Goose &nbsp;
+              
+              <a
+            href="https://twitter.com/goosepunks"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={Twitter} alt="twitter" />
+          </a>
+              
+              </p>
               <span>
                 {/* Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                 Aliquam corrupti similique. */}
