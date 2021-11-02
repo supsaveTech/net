b2aa52a589615b5a31940c486547b06ef94004ee
Mitchel Charles (SupsaveTech) <63431390+supsaveTech@users.noreply.github.com>
2021-11-02 08:29:53 +0100

    Update index.js

--- src/sections/teams/index.js
+++ src/sections/teams/index.js
@@ -4,7 +4,7 @@ import Img2 from "../../assets/images/34.png";
 import Img3 from "../../assets/images/27.png";
 import Img4 from "../../assets/images/img8.png";
 import Img5 from "../../assets/images/img2.png";
-import Img6 from "../../assets/images/24.png";
+import Img6 from "../../assets/images/img10.png";
 import Twitter from "../../assets/images/twitter.png";
 import Discord from "../../assets/images/discord.png";
 import "./style.css";
