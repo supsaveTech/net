be259c69560211f2cc67d6017eeaecbb8d64793a
Mitchel Charles (SupsaveTech) <63431390+supsaveTech@users.noreply.github.com>
2021-11-03 17:23:40 +0100

    Update index.js

--- src/sections/roadmap/index.js
+++ src/sections/roadmap/index.js
@@ -1,39 +1,52 @@
 import React from "react";
 import "./style.css";
 import Roadmap from "../../assets/images/roadmap-line.jpg";
+import roadmapPoint from "../../assets/images/roadmap-point.svg";
 const Index = () => {  
   return (
     <section class="road-map section-space" id="roadmap">
       <div class="container">
         <div class="section-heading">
-          <h2 className="text-center">Utility Roadmap</h2>
-        </div>
+          <h2 className="text-center">GoosePunks Roadmap <br /> 🦢🛤️🧭</h2><br />
+        </div>   
         <div class="map-chart d-none d-lg-block">
           <span>
             <img src={Roadmap} alt="" />
           </span>
           <div class="mapbox-1 wow fadeInLeft">
             <div class="mapbox-inner">
-              <h4>Q1/U1: Goosepunks' Lore</h4>
-              <p>This quarter shall feature the launch of Goosepunks Lore,
-                an Artifical Intelligence (AI) powered utility that would enable all
-                goose create a backdrop story to their NFT. Stay tuned, stay updated. 
-                More announcements will be communicated in due course.
-              </p>
-              <a
-                href="https://www.app.goosepunks.com"
-                target="_blank"
-                rel="noreferrer"
-              >
-                https://www.app.goosepunks.com
-              </a>
+              <h4>
+              {/* <img
+              className="roadmap-point3"
+              src={roadmapPoint}
+              alt="roadmapPoint"
+              />&nbsp; */}
+              Q1: ✔️
+              </h4>
+              
+             <li> Website development. </li> 
+             <li>  Community growth on social channels. </li>   
+             <li> Airdrop and giveaway campaign. </li>
+             <li> 3,500 Tier 1 Renegade GoosePunk NFTs minted. </li>
+              
             </div>
           </div>
           <div class="mapbox-2 wow fadeInRight">
             <div class="mapbox-inner">
-              <h4>Q2/U2: Goosepunks' Marketplace</h4>
-              <p>This quarter shall feature the launch of Goosepunks Marketplace,
-                a market where all goose can mint their NFTs as well as upload and sell their NFTs.</p>
+            <h4>
+              <img
+              className="roadmap-point3"
+              src={roadmapPoint}
+              alt="roadmapPoint"
+            />&nbsp;Q1/U1: Goosepunks' Lore
+                </h4>
+              <p>
+              
+                This quarter shall feature the launch of Goosepunks Lore,
+                an Artifical Intelligence (AI) powered utility that would enable all
+                goose create a backdrop story to their NFT. Stay tuned, stay updated. 
+                More announcements will be communicated in due course.
+              </p>
               <a href="https://www.app.goosepunks.com" target="_blank" rel="noreferrer">
               https://www.app.goosepunks.com
               </a>
@@ -41,68 +54,183 @@ const Index = () => {
           </div>
           <div class="mapbox-3 wow fadeInLeft">
             <div class="mapbox-inner" target="_blank">
-              <h4>Q2/U3: Goosepunks' Gaggle</h4>
-              <p>We shall be launching the Goosepunks Gaggle in this quarter also,
-                 an interactive platfrom where all goose can come together to have fun, learn from one another
-                 and most importantly just GAGGLE.</p>
-              <a href="https://www.app.goosepunks.com">https://www.app.goosepunks.com</a>
+            <h4>Q2:</h4>
+              
+             <li> 3,500 Tier 2 Renegade GoosePunk NFTs minted. </li> 
+             <li> 3,000 Tier 3 Renegade GoosePunk NFTs minted. </li>   
+             <li> 2,000 Random Tier 1-3 GoosePunk NFTs minted. </li>
+             <li> Staking development initiation. </li>
+              
             </div>
           </div>
           <div class="mapbox-4 wow fadeInRight">
             <div class="mapbox-inner">
-              <h4>Q3/U4: Goosepunks' Rarible Integration</h4>
-              <p> Who doesn't love easter eggs, we would let you guess what Goosepunks' Rarible Integration
-                would be all about. But fear not for this goose shall continue to drop hints about that as we progress in due course...
-                Goose Gaggles
+              <h4><img
+              className="roadmap-point3"
+              src={roadmapPoint}
+              alt="roadmapPoint"
+            />&nbsp;Q2/U2: Goosepunks' Marketplace</h4>
+              <p> This quarter shall feature the launch of Goosepunks Marketplace, 
+                a market where all goose can mint their NFTs as well as upload and sell their NFTs.
               </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="row map-mobile d-lg-none">
-          <div className="col-12">
-            <div className="map-box map-box-1">
-            <h4>Q1/U1: Goosepunks' Lore</h4>
-              <p>This quarter shall feature the lunch of Goosepunks Lore,
-                an Artifical Intelligence (AI) powered utility that would enable all
-                goose create a backdrop story to their NFT. Stay tuned, stay updated. 
-                More announcements will be communicated in due course.
-              </p>
-              <a
-                href="https://www.app.goosepunks.com"
-                target="_blank"
-                rel="noreferrer"
-              >
-                https://www.app.goosepunks.com
-              </a>
-            </div>
-          </div>
-          <div className="col-12">
-            <div className="map-box map-box-2">
-            <h4>Q2/U2: Goosepunks' Marketplace</h4>
-              <p>This quarter shall feature the lunch of Goosepunks Marketplace,
-                a market where all goose can mint their NFTs as well as upload and sell their NFTs.</p>
               <a href="https://www.app.goosepunks.com" target="_blank" rel="noreferrer">
               https://www.app.goosepunks.com
               </a>
             </div>
           </div>
+          <div class="mapbox-5 wow fadeInLeft">
+            <div class="mapbox-inner" target="_blank">
+            <h4>Q3:</h4>
+              
+             <li> Community art collab contests. </li> 
+             <li> Development of Drifter GoosePunk NFTs. </li>   
+             <li> Staking Implementation. </li>
+             <li> Release of Drifter GoosePunk NFTs. </li>
+            </div>
+          </div>
+          <div class="mapbox-6 wow fadeInRight">
+            <div class="mapbox-inner">
+              <h4><img
+              className="roadmap-point3"
+              src={roadmapPoint}
+              alt="roadmapPoint"
+            />&nbsp;Q3/U3: Goosepunks' Gaggle</h4>
+              <p> We shall be launching the Goosepunks Gaggle in this quarter also, 
+                an interactive platfrom where all goose can come together to have fun, 
+                learn from one another and most importantly just GAGGLE.
+              </p>
+              <a href="https://www.app.goosepunks.com" target="_blank" rel="noreferrer">
+              https://www.app.goosepunks.com
+              </a>
+            </div>
+          </div>
+          <div class="mapbox-7 wow fadeInLeft">
+            <div class="mapbox-inner" target="_blank">
+            <h4>Q4:</h4>
+              
+             <li> GoosePunks game development starts. </li> 
+             <li> Development of “The Swans” GoosePunk NFTs. </li>   
+             <li> Airdrop and giveaway campaign. </li>
+             <li> To be continued… </li>
+            </div>
+          </div>
+          <div class="mapbox-8 wow fadeInRight">
+            <div class="mapbox-inner">
+              <h4><img
+              className="roadmap-point3"
+              src={roadmapPoint}
+              alt="roadmapPoint"
+            />&nbsp;Q4/U4: Goosepunks' Rarible Integration</h4>
+              <p> Who doesn't love easter eggs, we would let you guess what Goosepunks' Rarible Integration
+                would be all about. But fear not for this goose shall continue to drop hints about 
+                that as we progress in due course... Goose Gaggles.
+              </p>
+              <a href="https://www.app.goosepunks.com" target="_blank" rel="noreferrer">
+              https://www.app.goosepunks.com
+              </a>
+            </div>
+          </div>
+
+        </div>
+
+        <div className="row map-mobile d-lg-none">
           <div className="col-12">
-            <div className="map-box map-box-3">
-            <h4>Q2/U3: Goosepunks' Gaggle</h4>
-              <p>We shall be launching the Goosepunks Gaggle in this quarter also,
-                 an interactive platfrom where all goose can come together to have fun, learn from one another
-                 and most importantly just GAGGLE.</p>
-              <a href="https://www.app.goosepunks.com">https://www.app.goosepunks.com</a>
+            <div className="map-box map-box-1">
+            <h4>Q1: ✔️</h4>
+            <li> Website development. </li> 
+             <li>  Community growth on social channels. </li>   
+             <li> Airdrop and giveaway campaign. </li>
+             <li> 3,500 Tier 1 Renegade GoosePunk NFTs minted. </li>
             </div>
           </div>
           <div className="col-12">
-            <div className="map-box ">
-            <h4>Q3/U4: Goosepunks' Rarible Integration</h4>
-              <p> Who doesn't love easter eggs, we would let you guess what Goosepunks' Rarible Integration
-                would be all about. But fear not for this goose shall continue to drop hints about that as we progress in due course...
-                Goose Gaggles
+            <div className="map-box map-box-2">
+            <h4>
+              <img
+              className="roadmap-point3"
+              src={roadmapPoint}
+              alt="roadmapPoint"
+            />&nbsp;Q1/U1: Goosepunks' Lore
+                </h4>
+                <p>
+              
+              This quarter shall feature the launch of Goosepunks Lore,
+              an Artifical Intelligence (AI) powered utility that would enable all
+              goose create a backdrop story to their NFT. Stay tuned, stay updated. 
+              More announcements will be communicated in due course.
+            </p>
+            <a href="https://www.app.goosepunks.com" target="_blank" rel="noreferrer">
+            https://www.app.goosepunks.com
+            </a>
+            </div>
+          </div>
+          <div className="col-12">
+            <div className="map-box map-box-3">
+            <h4>Q2:</h4>
+              
+             <li> 3,500 Tier 2 Renegade GoosePunk NFTs minted. </li> 
+             <li> 3,000 Tier 3 Renegade GoosePunk NFTs minted. </li>   
+             <li> 2,000 Random Tier 1-3 GoosePunk NFTs minted. </li>
+             <li> Staking development initiation. </li>
+            </div>
+          </div>
+          <div className="col-12">
+            <div className="map-box map-box-4">
+            <h4><img
+              className="roadmap-point3"
+              src={roadmapPoint}
+              alt="roadmapPoint"
+            />&nbsp;Q2/U2: Goosepunks' Marketplace</h4>
+              <p> This quarter shall feature the launch of Goosepunks Marketplace, 
+                a market where all goose can mint their NFTs as well as upload and sell their NFTs.
               </p>
+              <a href="https://www.app.goosepunks.com" target="_blank" rel="noreferrer">
+              https://www.app.goosepunks.com
+              </a>
+            </div>
+            <div className="map-box map-box-5">
+            <h4>Q3:</h4>
+              
+             <li> Community art collab contests. </li> 
+             <li> Development of Drifter GoosePunk NFTs. </li>   
+             <li> Staking Implementation. </li>
+             <li> Release of Drifter GoosePunk NFTs. </li>
+            </div>
+            <div className="map-box map-box-6">
+            <h4><img
+              className="roadmap-point3"
+              src={roadmapPoint}
+              alt="roadmapPoint"
+            />&nbsp;Q3/U3: Goosepunks' Gaggle</h4>
+              <p> We shall be launching the Goosepunks Gaggle in this quarter also, 
+                an interactive platfrom where all goose can come together to have fun, 
+                learn from one another and most importantly just GAGGLE.
+              </p>
+              <a href="https://www.app.goosepunks.com" target="_blank" rel="noreferrer">
+              https://www.app.goosepunks.com
+              </a>
+            </div>
+            <div className="map-box map-box-7">
+            <h4>Q4:</h4>
+              
+             <li> GoosePunks game development starts. </li> 
+             <li> Development of “The Swans” GoosePunk NFTs. </li>   
+             <li> Airdrop and giveaway campaign. </li>
+             <li> To be continued… </li>
+            </div>
+            <div className="map-box map-box-8">
+            <h4><img
+              className="roadmap-point3"
+              src={roadmapPoint}
+              alt="roadmapPoint"
+            />&nbsp;Q4/U4: Goosepunks' Rarible Integration</h4>
+              <p> Who doesn't love easter eggs, we would let you guess what Goosepunks' Rarible Integration
+                would be all about. But fear not for this goose shall continue to drop hints about 
+                that as we progress in due course... Goose Gaggles.
+              </p>
+              <a href="https://www.app.goosepunks.com" target="_blank" rel="noreferrer">
+              https://www.app.goosepunks.com
+              </a>
             </div>
           </div>
         </div>
