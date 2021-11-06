83e7a3a05dc42fad8ae9f295f231809bbbd30139
Mitchel Charles (SupsaveTech) <63431390+supsaveTech@users.noreply.github.com>
2021-11-06 01:01:56 +0100

    Update index.js

--- src/sections/traitSection/index.js
+++ src/sections/traitSection/index.js
@@ -1,89 +1,107 @@
 import React from "react";
 import "./style.css";
-import Bear6 from "../../assets/images/bear-6.png";
-import Element1 from "../../assets/images/element-bg.png";
-import Element2 from "../../assets/images/element-bg2.png";
-import Element3 from "../../assets/images/element-bg3.png";
-import Element4 from "../../assets/images/element-bg4.png";
-const Index = () => {
+import { useState } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import logo from '../../assets/images/goose.png';
+
+const Index = ({ searchQuery, setSearchQuery })  => {
+    const [apiData, setApiData] = useState();
+    const [ apiR, setApiR] = useState();
+
+const fetchData = async (e)=>{
+        e.preventDefault();
+        try{
+
+            const response = await fetch(' https://api.openai.com/v1/engines/babbage/completions', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': 'Bearer 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',
+            },
+            body: JSON.stringify({
+                "prompt": apiData,
+                "temperature": 0.7,
+                "max_tokens": 400,
+                "top_p": 0.11,
+                "frequency_penalty": 1.23,
+                "presence_penalty": 1.22
+            }),
+        });
+
+        const data = await response.json();
+
+            if (data){
+                setApiR(data.choices[0].text);
+                console.log(data.choices[0].text);
+            }
+        }catch(e){
+            console.log(e.message);
+        }
+    }
+
   return (
-    <section className="section-space" id="traits">
-      <div className="container">
-        <div class="section-heading">
-          <h2>Traits</h2>
+    <Router>
+            <div className="App section-space" id="lore">
+            <div class="section-heading container">
+          <h2 className="text-center">GoosePunks Lore <br /> 🦢</h2><br />
+        </div> 
+                <img src={logo} className="App-logo" alt="logo" />
+                <br />
+    <form  
+        action="/"
+        method="get"
+        autoComplete="on"
+        onSubmit={fetchData}
+        className="text-center"
+    >
+        <label htmlFor="header-search">
+            <span className="visually-hidden">
+                Generate your own GoosePunks' Lore
+            </span>
+        </label>
+        <div class="input-group input-group-lg container">
+        <input
+            type="text"
+            // id="header-search"
+            placeholder="Create ur GoosePunks' Lore"
+            class="form-control"
+            autoComplete="on"
+            aria-label="Large" 
+            value={apiData}
+            onChange={(e) => setApiData(e.target.value)}
+        />
+        <div class="input-group-append">
+        <button class="btn btn-outline-secondary" type="submit" value="send" data-toggle="modal" data-target="#exampleModal">Create 🦢</button>
         </div>
-        <div className="row align-items-center justify-content-between traits">
-          <div class="col-12 col-md-4">
-            <div class="traits-list ">
-              <ul>
-                <li>
-                  <span>11</span> Backgrounds
-                </li>
-                <li>
-                  <span>45</span> Skins{" "}
-                </li>
-                <li>
-                  <span>37</span> Pants
-                </li>
-                <li>
-                  <span>80</span> Shirts
-                </li>
-                <li>
-                  <span>18</span> Mouths
-                </li>
-                <li>
-                  <span>18</span> Eyes
-                </li>
-                <li>
-                  <span>21</span> Glasses
-                </li>
-                <li>
-                  <span>75</span> Shoes
-                </li>
-                <li>
-                  <span>57</span> Headwear
-                </li>
-                <li>
-                  <span>18</span> Accessories
-                </li>
-              </ul>
-              <button class="total-traits">
-                <span>383</span> Total Traits
-              </button>
-            </div>
-          </div>
-          <div class="col-md-8  traitImage d-none d-md-block ">
-            <div class="traits-box ">
-              <div class="bear-modal">
-                <div class="modalimg">
-                  <img src={Bear6} alt="" />
-                </div>
-
-                {/* <!-- modal elements --> */}
-
-                <div class="modal-elements element-1">
-                  {/* <!-- <span><img src="images/cap@2x.png"></span> --> */}
-                  <img src={Element1} alt="" />
-                </div>
-
-                <div class="modal-elements element-2">
-                  <img src={Element2} alt="" />
-                </div>
-
-                <div class="modal-elements element-3">
-                  <img src={Element3} alt="" />
-                </div>
-
-                <div class="modal-elements element-4">
-                  <img src={Element4} alt="" />
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
+    </form>
+    {/* Modal */}
+  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
       </div>
-    </section>
-  );
-};
+      <div class="modal-body">
+                {/* {apiR} */}
+                <h5>
+                {apiR ? apiR : "loading..."}
+                </h5>
+      </div>
+      <div class="modal-footer"> 
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        {/* <button type="button" class="btn btn-primary">Save changes</button> */}
+      </div>
+    </div>
+  </div>
+</div>
+    
+            </div>
+        </Router>
+);
+  };
 
 export default Index;
