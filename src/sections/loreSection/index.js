639c47407d1bc423b63295b8e94dae5969e53af6
Mitchel Charles (SupsaveTech) <63431390+supsaveTech@users.noreply.github.com>
2021-11-07 14:32:44 +0100

    Update index.js

--- src/sections/loreSection/index.js
+++ src/sections/loreSection/index.js
@@ -24,8 +24,7 @@ const fetchData = async (e)=>{
                 "max_tokens": 188,
                 "top_p": 1,
                 "frequency_penalty": 0,
-                "presence_penalty": 0,
-                "stop": ["...."]
+                "presence_penalty": 0
 
             }),
         });
